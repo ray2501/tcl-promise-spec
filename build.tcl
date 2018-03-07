906afed53a9a086bdd8253d6a923ee13050f2bdc
Danilo Chang <ray2501@gmail.com>
2018-03-07 21:31:47 +0800

    Get file to build

--- build.tcl
+++ build.tcl
@@ -3,6 +3,20 @@
 set arch "noarch"
 set base "promise-1.0.3"
 
+set fileurl "https://sourceforge.net/projects/tcl-promise/files/promise-1.0.3.tm"
+
+set var [list wget $fileurl -O $base.tm]
+exec >@stdout 2>@stderr {*}$var
+
+file mkdir $base
+cd $base
+file copy ../$base.tm .
+cd ..
+
+file delete -force $base.tm
+set var [list tar czvf $base.tar.gz $base]
+exec >@stdout 2>@stderr {*}$var
+
 if {[file exists build]} {
     file delete -force build
 }
@@ -12,4 +26,8 @@ file copy -force $base.tar.gz build/SOURCES
 
 set buildit [list rpmbuild --target $arch --define "_topdir [pwd]/build" -bb tcl-promise.spec]
 exec >@stdout 2>@stderr {*}$buildit
+
+#Remove source package
+file delete -force $base
+#file delete -force $base.tar.gz
 
